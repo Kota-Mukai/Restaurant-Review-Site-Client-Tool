79fde93cf3068b9f329b2847dac4ea71fc865810
Kota_Mukai <kota1022@mukai@gmail.com>
2025-10-05 22:51:16 +0900

    pull request #5

---
+++ src/main/java/com/example/demo/service/ReviewDeleteService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Review;
+
+public interface ReviewDeleteService {
+	
+	void remove(Review review);
+
+}
---
+++ src/main/java/com/example/demo/service/ReviewEditService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Review;
+
+public interface ReviewEditService {
+	
+	void edit(Review review);
+
+}
